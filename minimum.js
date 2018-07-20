d259c89ef6cf4d796f2f464ca36b19528f174cf2
Tobi <owner@Owners-MacBook-Air.local>
2018-07-20 12:44:23 +0100

    completed exercise

---
+++ minimum.js
@@ -0,0 +1,11 @@
+//The aim of this project is to write a function min that takes two arguments and returns their minimum
+
+function min (a,b){
+	if (a < b){
+		console.log(a);
+	} else {
+		console.log(b);
+	}
+}
+
+min(5,2) // b should be returned as it is less than a.
